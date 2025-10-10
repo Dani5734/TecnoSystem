5049efee0402d7e0de5721f14b136f3e8a8d5c87
Esteban-1890 <estebanromerom18@gmail.com>
2025-10-09 21:40:13 -0600

    Cambios en el usuario

--- model/Usuarios.php
+++ model/Usuarios.php
@@ -112,7 +112,7 @@ class Usuarios
         session_start();
         session_unset();
         session_destroy();
-        header("Location: ../index.php");
+        header("Location: ../index.html");
         exit();
     }
 
